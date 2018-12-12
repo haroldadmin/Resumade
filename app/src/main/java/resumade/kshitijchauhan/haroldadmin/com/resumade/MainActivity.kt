dfe5e50096f8dc0ea33f7dbd039e82c850f8c475
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-12-12 16:55:44 +0530

    Initialize empty project

---
+++ app/src/main/java/resumade/kshitijchauhan/haroldadmin/com/resumade/MainActivity.kt
@@ -0,0 +1,12 @@
+package resumade.kshitijchauhan.haroldadmin.com.resumade
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
