ce84603d3d69e81f27128406f5ce9a6df52ca120
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-12-21 09:55:47 +0530

    misc: Migrate to androidX

--- app/src/main/java/resumade/kshitijchauhan/haroldadmin/com/resumade/MainActivity.kt
+++ app/src/main/java/resumade/kshitijchauhan/haroldadmin/com/resumade/MainActivity.kt
@@ -1,6 +1,6 @@
 package resumade.kshitijchauhan.haroldadmin.com.resumade
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
